fd92c96ede65f5b607d145ee88f305a3d9e14631
Trần Anh Tuấn <19522481@gm.uit.edu.vn>
2021-05-14 11:38:58 +0700

    mainactivities + 3 fragments

--- Calories/app/src/androidTest/java/com/example/calories/ExampleInstrumentedTest.java
+++ Calories/app/src/androidTest/java/com/example/calories/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.caloriesapp;
+package com.example.calories;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.caloriesapp", appContext.getPackageName());
+        assertEquals("com.example.calories", appContext.getPackageName());
     }
 }
