5c489dfe54714b7d497eadf1c8bb8b09abda33d5
ThanhTuan <72434152+thanhtuan2503-uit@users.noreply.github.com>
2021-07-03 22:21:34 +0700

    update

--- CaloriesApp/app/src/main/java/com/example/caloriesapp/fragment/FragmentAccount.java
+++ CaloriesApp/app/src/main/java/com/example/caloriesapp/fragment/FragmentAccount.java
@@ -1,11 +1,15 @@
 package com.example.caloriesapp.fragment;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,11 +18,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
-import com.example.caloriesapp.Foodate;
+
 import com.example.caloriesapp.R;
 import com.example.caloriesapp.User;
 import com.example.caloriesapp.activities.LoginActivity;
+import com.example.caloriesapp.activities.MainActivity;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
@@ -27,8 +31,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Date;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 import es.dmoral.toasty.Toasty;
+
+import static com.example.caloriesapp.activities.Notification.CHANNEL_ID;
 
 
 public class FragmentAccount extends Fragment {
@@ -37,7 +45,8 @@ public class FragmentAccount extends Fragment {
     private CircleImageView profile;
     private FloatingActionButton changeProfile;
     private View view;
-    private TextView userName, male, age, height, currentWeight,goalWeight;
+    private TextView userName, male, age, height, currentWeight,goalWeight, purpose;
+    boolean Click;
     ImageButton notification;
     Button logout;
 
@@ -46,6 +55,8 @@ public class FragmentAccount extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         view = inflater.inflate(R.layout.fragment_account, container, false);
+
+        Click =false;
 
         AnhXa();
         syncUserWithFirebase();
@@ -66,7 +77,47 @@ public class FragmentAccount extends Fragment {
                 goToAttract(view);
             }
         });
+
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification();
+
+                if(Click==false)
+                {
+                    notification.setImageResource(R.drawable.ic_baseline_notifications_off_24);
+                }
+                else
+                    notification.setImageResource(R.drawable.ic_baseline_notifications);
+            }
+        });
         return view;
+    }
+
+
+    private void sendNotification() {
+
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(),0, intent,0);
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getActivity(),CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_water_notification)
+                .setContentTitle("Calories App")
+                .setContentText("Đã đến lúc uống nước!")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("Đã đến lúc uống nước!"))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getActivity());
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(getNotificationId(), notification.build());
+
+    }
+    private int getNotificationId(){
+        return (int) new Date().getTime();
     }
 
     @Override
@@ -84,18 +135,22 @@ public class FragmentAccount extends Fragment {
         userName = view.findViewById(R.id.txtUsername);
         male = view.findViewById(R.id.textsex);
         age = view.findViewById(R.id.textage);
-        height = view.findViewById(R.id.txtheight);
+        height = view.findViewById(R.id.textheight);
         currentWeight = view.findViewById(R.id.textcurrentweight);
         goalWeight = view.findViewById(R.id.textgoalweight);
         notification = view.findViewById(R.id.imb_notification);
         logout = view.findViewById(R.id.btnLogin);
+        purpose = view.findViewById(R.id.txtpurpose);
 
     }
 
     public void goToAttract(View v)
     {
+        FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         startActivity(intent);
+        getActivity().finish();
+
     }
 
     private void syncUserWithFirebase() {
@@ -111,11 +166,13 @@ public class FragmentAccount extends Fragment {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 user = snapshot.getValue(User.class);
                 userName.setText(user.getUserName());
-                age.setText(user.getAge());
+                age.setText(String.valueOf(user.getAge()));
                 male.setText(user.getGender());
-                height.setText((int) user.getHeight());
-                currentWeight.setText(user.getPurposeWeight());
-                goalWeight.setText((int) user.getGoalWeight());
+                height.setText(String.valueOf(user.getHeight()) + " CM" );
+                currentWeight.setText(String.valueOf(user.getCurrentWeight()) +" KG" );
+                goalWeight.setText(String.valueOf(user.getGoalWeight()) + " KG");
+                purpose.setText(user.getPurposeWeight());
+
 
 
                 // update ui here with user
